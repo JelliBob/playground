3d9e87737cdf8e02c1591eca761e9de953f22765
JelliBob <fgetc@naver.com>
2021-09-01 10:12:04 +0900

    UserMapper 매개변수명 수정

--- src/main/java/com/cos/playground/mapper/UserMapper.java
+++ src/main/java/com/cos/playground/mapper/UserMapper.java
@@ -17,7 +17,7 @@ public interface UserMapper {
 	void updateInfo(User user); // 회원 정보 수정하기
 	void delete(User user); // 회원 탈퇴
 	void likePost(int param1, int param2); // 게시글 좋아요 누르기
-	void cancleFav(int userId, int boardId); // 게시글 좋아요 취소하기
+	void cancleFav(int param1, int param2); // 게시글 좋아요 취소하기
 	Fav favHistory(int param1, int param2); // 해당글 좋아요 내역이 있는지 확인
 	List<CBoard> favList(int userId); // 좋아요 누른 게시글 목록 조회하기
 	List<CBoard> myPostList(int userId); // 작성한 글 목록 보기
